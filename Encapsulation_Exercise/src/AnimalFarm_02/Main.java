27b5b9e23ca917e154b1a6de56e498816013941f
Nikola-Drinkov <nikola13drinkov@gmail.com>
2023-02-27 23:49:05 +0200

    Solved problem

---
+++ Encapsulation_Exercise/src/AnimalFarm_02/Main.java
@@ -0,0 +1,13 @@
+package AnimalFarm_02;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name = sc.nextLine();
+        int age = Integer.parseInt(sc.nextLine());
+        Chicken chicken = new Chicken(name,age);
+        System.out.println(chicken);
+    }
+}
