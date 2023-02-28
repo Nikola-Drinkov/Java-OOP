441fcf2ba2d2fc489378e64341732a95600dd485
Nikola-Drinkov <nikola13drinkov@gmail.com>
2023-03-01 00:24:42 +0200

    Solved problem

--- Inheritance/src/RandomArrayList/RandomArrayList.java
+++ Inheritance/src/RandomArrayList/RandomArrayList.java
@@ -4,13 +4,12 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class RandomArrayList<T> extends ArrayList<T> {
-    private Random random;
 
     public RandomArrayList() {
         super();
     }
     public T getRandomElement(){
-        random = new Random();
+        Random random = new Random();
         int index = random.nextInt(super.size());
         T element = super.get(index);
         super.remove(element);
