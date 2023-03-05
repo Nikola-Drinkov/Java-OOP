891161adb8d376a5f933262242fb0bcf2cd2013d
Nikola-Drinkov <nikola13drinkov@gmail.com>
2023-03-06 00:32:33 +0200

    Solved problem

---
+++ Interfaces/src/SayHelloExtend/BasePerson.java
@@ -0,0 +1,18 @@
+package SayHelloExtend;
+
+public abstract class BasePerson implements Person{
+    private String name;
+    protected BasePerson(String name){
+        this.name = name;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public String sayHello() {
+        return "Hello!";
+    }
+}
---
+++ Interfaces/src/SayHelloExtend/Bulgarian.java
@@ -0,0 +1,11 @@
+package SayHelloExtend;
+
+public class Bulgarian extends BasePerson {
+    public Bulgarian(String name) {
+        super(name);
+    }
+    @Override
+    public String sayHello() {
+        return "Здравей!";
+    }
+}
