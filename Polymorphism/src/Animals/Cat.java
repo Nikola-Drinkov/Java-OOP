30804d9986ccab7fc1976d836987ec67868879f7
Nikola-Drinkov <nikola13drinkov@gmail.com>
2023-03-08 01:02:48 +0200

    Solved problem

---
+++ Polymorphism/src/Animals/Cat.java
@@ -0,0 +1,12 @@
+package Animals;
+
+public class Cat extends Animal{
+    public Cat(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    String explainSelf() {
+        return String.format("I am %s and my favourite food is %s%nMEEOW",this.getName(),this.getFavouriteFood());
+    }
+}
---
+++ Polymorphism/src/Animals/Dog.java
@@ -0,0 +1,12 @@
+package Animals;
+
+public class Dog extends Animal{
+    public Dog(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    String explainSelf() {
+        return String.format("I am %s and my favourite food is %s%nDJAAF",this.getName(),this.getFavouriteFood());
+    }
+}
